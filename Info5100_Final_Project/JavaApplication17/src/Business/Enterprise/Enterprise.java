f8c789614e1358ca4f5856bc33a616bbdcb9d157
Rudra1723 <patel.rudraa@northeastern.edu>
2025-05-29 12:04:56 -0400

    Update in code

--- Info5100_Final_Project/JavaApplication17/src/Business/Enterprise/Enterprise.java
+++ Info5100_Final_Project/JavaApplication17/src/Business/Enterprise/Enterprise.java
@@ -4,10 +4,55 @@
  */
 package Business.Enterprise;
 
+import Business.Organization.Organization;
+import Business.Organization.OrganizationDirectory;
+
 /**
  *
  * @author rudrapatel
  */
-public class Enterprise {
+public abstract class Enterprise extends Organization{
     
+    private EnterpriseType enterpriseType;
+    private OrganizationDirectory organizationDirectory;
+
+     public Enterprise(String name,EnterpriseType type){
+        super(name);
+        this.enterpriseType=type;
+        organizationDirectory=new OrganizationDirectory();
+    }
+    
+    
+    public OrganizationDirectory getOrganizationDirectory() {
+        return organizationDirectory;
+    }
+    
+    public enum EnterpriseType{
+        Agriculture("Agriculture"),
+        Government("Government"),
+        Hospital("Hospital");
+        
+        private String value;
+        
+        private EnterpriseType(String value){
+            this.value=value;
+        }
+        public String getValue() {
+            return value;
+        }
+        @Override
+        public String toString(){
+        return value;
+    }
+    }
+
+    public EnterpriseType getEnterpriseType() {
+        return enterpriseType;
+    }
+
+    public void setEnterpriseType(EnterpriseType enterpriseType) {
+        this.enterpriseType = enterpriseType;
+    }
+    
+   
 }

1e0bc9186cd80ba0d72b5333084e5c522108d861
Rudra1723 <patel.rudraa@northeastern.edu>
2025-05-29 11:18:09 -0400

    Implemented OfficerRole java class.

---
+++ Info5100_Final_Project/JavaApplication17/src/Business/Role/OfficerRole.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userinterface.Government.OfficerWorkAreaJPanel;
+
+/**
+ *
+ * @author raunak
+ */
+public class OfficerRole extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new OfficerWorkAreaJPanel(userProcessContainer, account, organization, business);
+    }
+    
+}

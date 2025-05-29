d5ed6b97a17b472bf818273872ac4bc4bf08f705
Rudra1723 <patel.rudraa@northeastern.edu>
2025-05-29 11:24:25 -0400

    Implemented DoctorRole java class.

---
+++ Info5100_Final_Project/JavaApplication17/src/DoctorRole.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+package Business.Role;
+
+import userinterface.Doctor.RequestDoctorTreatmentJPanel;
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author rudrapatel
+ */
+public class DoctorRole extends Role{
+    
+     public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new RequestDoctorTreatmentJPanel(userProcessContainer, account, organization, business);
+    }
+}

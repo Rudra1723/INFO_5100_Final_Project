91e6152ea0fb40a9909c467d7688e9b1332738ef
Rudra1723 <patel.rudraa@northeastern.edu>
2025-05-29 11:56:27 -0400

    Added FarmerEventsWorkRequest class

---
+++ Info5100_Final_Project/JavaApplication17/src/Business/WorkQueue/FarmerEventsWorkRequest.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.WorkQueue;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+/**
+ *
+ * @author rudrapatel
+ */
+public class FarmerEventsWorkRequest extends WorkRequest{
+    
+    private String eventName;
+    private String eventDesc;
+    private String location;
+    private Date eventDate;
+
+    
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventDesc() {
+        return eventDesc;
+    }
+
+    public void setEventDesc(String eventDesc) {
+        this.eventDesc = eventDesc;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(Date eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    @Override
+    public String toString() {
+        return eventName;
+    }
+    
+    
+}

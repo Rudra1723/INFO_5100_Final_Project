896d24264a19a614248fa5266bf87164dcd7e569
Rudra1723 <patel.rudraa@northeastern.edu>
2025-05-29 11:46:07 -0400

    Implemented DistributorOrganization java class

---
+++ Info5100_Final_Project/JavaApplication17/src/Business/Organization/DistributorOrganization.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Organization;
+
+import Business.Role.DistributorRole;
+import Business.Role.ExpertRole;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rudrapatel
+ */
+public class DistributorOrganization extends Organization{
+
+    public DistributorOrganization() {
+        super(Type.Distributor.getValue());
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new DistributorRole());
+        return roles;
+    }
+     
+   
+    
+    
+}

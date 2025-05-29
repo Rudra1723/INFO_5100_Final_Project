d2c737df47e4c3f93166228cc3e6e119bfba1362
Rudra1723 <patel.rudraa@northeastern.edu>
2025-05-29 12:08:25 -0400

    Added Farmer package

---
+++ Info5100_Final_Project/JavaApplication17/src/Business/Farmer/Farmer.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Farmer;
+
+import Business.Crop.Crop;
+import Business.OrganicCertification.OrganicCertification;
+import Business.Forum.*;
+/**
+ *
+ * @author rudrapatel
+ */
+public class Farmer {
+    private String name;
+    private String city;
+    private String state;
+    private String email;
+    private OrganicCertification organicCertification;
+    private Forum forum;
+ 
+    
+}
